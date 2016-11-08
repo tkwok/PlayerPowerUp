5ec17019d88a7e352d0fa06fbf187b8d42a42a71
Tony Kwok <kmunchr@gmail.com>
2016-11-08 13:05:22 -0800

    first checkin

---
+++ Assets/Script/ItemRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemRotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/* inline create vector to spin different speeds based on angle */
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/* ideal for game movement code here */
+	void FixedUpdate() {
+		/* define input horizontal, vertical - keyboard/controller */
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		/* calculate 3D Movement, class of x, y, z of vector in space */
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		/* fixed time update, at 60fps default, every object has transform, pointer
+		 * Time.deltaTime, time between last frame, code more consistent, smoother movement, 
+		 */
+		transform.Translate (movement * Time.deltaTime * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		/* if I collide with powerup, set active */
+		if (other.gameObject.CompareTag("PowerUp")){
+			other.gameObject.SetActive(false);
+		}
+	}
+}
